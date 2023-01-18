795e8fb226b6398762911fd7d959c03ff9711565
Hyemin Heo <heohm128@gmail.com>
2023-01-18 10:50:16 +0900

    Initial Commit

---
+++ CheckIt/CheckIt/CheckItApp.swift
@@ -0,0 +1,34 @@
+//
+//  CheckItApp.swift
+//  CheckIt
+//
+//  Created by sole on 2023/01/18.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+@main
+struct CheckItApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
