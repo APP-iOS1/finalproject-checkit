81a82c20ce8d938483f74fda3564e3e8215f178e
Cho Hyunho <chohh02@gmail.com>
2023-01-18 17:14:50 +0900

    #9 [Style] Group Folder
    동아리 탭 상단 카테고리 구현
    동아리 일정 뷰
    동아리 일정 추가하기 뷰
    동아리 출석 뷰 (개인)

---
+++ CheckIt/CheckIt/View/Group/CategoryView.swift
@@ -0,0 +1,57 @@
+//
+//  CategoryView.swift
+//  CheckIt
+//
+//  Created by 조현호 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    @State var clickedIndex: Int = 0
+    
+    let categories: [String] = ["동아리 일정", "출석부", "동아리 정보"]
+    
+    var body: some View {
+        VStack {
+            Text("허니미니의 또구 동아리")
+                .font(.title2)
+                .bold()
+                .padding()
+            
+            HStack {
+                ForEach(categories.indices) { i in
+                    Button(action: {
+                        clickedIndex = i
+                    }, label: {
+                        Text(categories[i])
+                            .foregroundColor(i == clickedIndex ? .black : .gray)
+                            .font(i == clickedIndex ? .system(size: 18).bold(): .system(size: 18))
+                    })
+                    .padding()
+                }
+            }
+            .frame(width: 330)
+            .border(Color("myGreen"))
+            .padding(.bottom, 20)
+            
+            if clickedIndex == 0 {
+                GroupScheduleView()
+            }
+            if clickedIndex == 1 {
+                AttendanceStatusView()
+            }
+            if clickedIndex == 2 {
+                Text("동아리 정보")
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView()
+    }
+}
