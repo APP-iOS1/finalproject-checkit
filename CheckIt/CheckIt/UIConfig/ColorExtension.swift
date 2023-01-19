851eda222aeb5a40f9fa9bd609d1937dc1ce611d
Hyemin Heo <heohm128@gmail.com>
2023-01-19 17:44:55 +0900

    [Style] 메인컬러 및 보조 컬러 변경

--- CheckIt/CheckIt/UIConfig/ColorExtension.swift
+++ CheckIt/CheckIt/UIConfig/ColorExtension.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 extension Color {
     static var myGreen = Color("myGreen")
-    static var myYellow = Color("myYellow")
+    static var myOrange = Color("myOrange")
     static var myRed = Color("myRed")
     static var myGray = Color("myGray")
     static var myLightGray = Color("myLightGray")
