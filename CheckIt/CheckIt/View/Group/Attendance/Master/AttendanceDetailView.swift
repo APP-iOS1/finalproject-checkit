e5fe249cef724878e0534422a9dd47e25940abe1
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-20 14:31:24 +0900

    #21 - [Chore] 출석 상태를 보여주는 picker를 AttendancePicker로 분리

--- CheckIt/CheckIt/View/Group/Attendance/Master/AttendanceDetailView.swift
+++ CheckIt/CheckIt/View/Group/Attendance/Master/AttendanceDetailView.swift
@@ -18,39 +18,18 @@ struct LateCost: Identifiable {
 struct AttendanceDetailView: View {
     @State private var selectedTap: AttendanceCategory = .attendanced
     
-//    var totalCost: Int {
-//        tempCostData
-//            .filter{ $0.isChecked }
-//            .map{ $0.cost }
-//            .reduce(0, +)
-//    }
+    //    var totalCost: Int {
+    //        tempCostData
+    //            .filter{ $0.isChecked }
+    //            .map{ $0.cost }
+    //            .reduce(0, +)
+    //    }
     
     var body: some View {
         VStack {
-            HStack {
-                ForEach(AttendanceCategory.allCases, id: \.self) { tap in
-                    VStack {
-                        Button {
-                            selectedTap = tap
-                        } label: {
-                            Text("\(tap.rawValue)")
-                                .foregroundColor(selectedTap == tap ? .black : .gray)
-                        }
-                        
-                        if selectedTap == tap {
-                            Capsule()
-                                .foregroundColor(.black)
-                                .frame(height: 2)
-                        } else {
-                            Capsule()
-                                .foregroundColor(.white)
-                                .frame(height: 2)
-                        }
-                    }
-                }
-            }
-            .padding()
-            .padding(.top, UIScreen.main.bounds.height / 30)
+            AttendancePickerView(selectedTap: $selectedTap)
+                .padding()
+                .padding(.top, UIScreen.main.bounds.height / 30)
             
             AttendanceCategoryView(selection: selectedTap)
             
