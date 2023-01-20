cb7eef4ee1258f8f7976e0d39d0d4a08659a2711
blaire-pi <gpwls1969@cau.ac.kr>
2023-01-20 14:12:32 +0900

    #24 - [Style] to add the picker background

--- CheckIt/CheckIt/View/Calendar/PickerView.swift
+++ CheckIt/CheckIt/View/Calendar/PickerView.swift
@@ -10,26 +10,35 @@ import SwiftUI
 struct PickerView: View {
     
     @State var selectedGroup = "전체"
+    //동아리 샘플 배열
     var groups = ["전체", "지니의 맛집 탐방", "호이의 SSG 응원방", "허니부리 또구 교실", "또리의 이력서 클럽dddddddddddddd"]
     
     var body: some View {
-        Menu {
-            Picker(selection: $selectedGroup) {
-                ForEach(groups, id: \.self) { group in
-                    Text(group)
-                        .tag(group)
-                }
-            } label: {}
-        } label: {
-            Text(selectedGroup)
-                .font(.body)
-                .lineLimit(1)
-        }.id(selectedGroup)
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+//                .frame(width: 120)
+                .foregroundColor(Color.myLightGray)
+//                .padding(.horizontal)
+            Menu {
+                Picker(selection: $selectedGroup) {
+                    ForEach(groups, id: \.self) { group in
+                        Text(group)
+                            .tag(group)
+                    }
+                } label: {}
+            } label: {
+                Text(selectedGroup)
+                    .font(.body)
+                    .lineLimit(1)
+            }.id(selectedGroup)
+                .frame(width: 100)
+        }
+        .frame(width: 130, height: 35)
     }
 }
 
 struct PickerView_Previews: PreviewProvider {
     static var previews: some View {
-        CalendarView()
+        PickerView()
     }
 }
