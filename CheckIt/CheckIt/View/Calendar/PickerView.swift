b155a42a80072eac3bcf5e8385cf662eef7679d8
Hyemin Heo <heohm128@gmail.com>
2023-01-20 17:02:04 +0900

    [Fix] 동아리 필터의 글씨 잘리는 현상 수정

--- CheckIt/CheckIt/View/Calendar/PickerView.swift
+++ CheckIt/CheckIt/View/Calendar/PickerView.swift
@@ -14,27 +14,24 @@ struct PickerView: View {
     var groups = ["전체", "지니의맛집", "지니의맛집탐", "지니의맛집탐방", "호이의 SSG 응원방", "허니부리 또구 교실", "또리의 이력서 클럽ㅇㅇ"]
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 5)
-//                .frame(width: 120)
-                .foregroundColor(Color.myLightGray)
-//                .padding(.horizontal)
-            Menu {
-                Picker(selection: $selectedGroup) {
-                    ForEach(groups, id: \.self) { group in
-                        Text(group)
-                            .tag(group)
-                    }
-                } label: {}
-            } label: {
-                Text(selectedGroup)
-                    .font(.body)
-                    .lineLimit(1)
-            }.id(selectedGroup)
-                .frame(width: 170)
+            ZStack {
+                RoundedRectangle(cornerRadius: 5)
+                    .foregroundColor(Color.myLightGray)
+                Menu {
+                    Picker(selection: $selectedGroup) {
+                        ForEach(groups, id: \.self) { group in
+                            Text(group)
+                                .tag(group)
+                        }
+                    } label: {}
+                } label: {
+                    Text(selectedGroup)
+                        .font(.body)
+                        .frame(width: 170)
+                }.id(selectedGroup)
+            }
+            .frame(width: 180, height: 35)
         }
-        .frame(width: 180, height: 35)
-    }
 }
 
 struct PickerView_Previews: PreviewProvider {
