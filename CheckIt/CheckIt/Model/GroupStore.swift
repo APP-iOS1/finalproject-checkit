7dfe1fa65828714f92d5cecefc6df4be44d821ef
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-31 17:05:54 +0900

    #44 - [Feat] 동아리를 생성하는 기능 구현

--- CheckIt/CheckIt/Model/GroupStore.swift
+++ CheckIt/CheckIt/Model/GroupStore.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseStorage
+
 
 class GroupStore: ObservableObject {
+    @Published var groups: [Group] = []
     
+    let database = Firestore.firestore().collection("Group")
+    
+    
+    // MARK: - 동아리를 개설하는 메소드이다.
+    /// - Parameter uid: 로그인한사용자의 uid 동아리 방장의 id
+    /// - Parameter group: 사용자가 생성한 동아리 인스턴스
+    func createGroup(_ uid: String, group: Group) async {
+        do {
+            try await database
+                .document(group.id)
+                .setData([
+                    "id": group.id,
+                    "host_id": group.hostID,
+                    "invitationCode": group.invitationCode,
+                    "image": group.image,
+                    "description": group.description,
+                    "schedule_id": group.scheduleID
+            ])
+        } catch {
+            print("동아리 생성 에러: \(error.localizedDescription)")
+        }
+    }
 }
+
