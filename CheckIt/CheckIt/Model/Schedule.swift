da4b13c2361674afffeb1cabf1317ad5c42d3f19
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-31 14:24:48 +0900

    [Feat] 데이터 모델 생성

---
+++ CheckIt/CheckIt/Model/Schedule.swift
@@ -0,0 +1,22 @@
+//
+//  Schedule.swift
+//  CheckIt
+//
+//  Created by 이학진 on 2023/01/31.
+//
+
+import Foundation
+
+struct Schedule {
+    var id: String
+    var groupName: String // 동아리명
+    var lateFee: Int
+    var absenteeFee: Int
+    var location: String
+    var startTime: Date // 2023년 4월 10일 15시 00
+    var endTime: Date  // 2023년 4월 10일 18시 00
+    var agreeTime: Int
+    var memo: String
+}
+
+
