4046b4836498d22046647923196791e6a099a504
hwangyeri <dpfl420@naver.com>
2023-02-01 19:03:50 +0900

    #64 - [Feat] 동아리 일정 데이터 연동 02

--- CheckIt/CheckIt/Model/ScheduleStore.swift
+++ CheckIt/CheckIt/Model/ScheduleStore.swift
@@ -14,8 +14,8 @@ class ScheduleStore: ObservableObject {
     let database = Firestore.firestore()
     
     // MARK: - fetchSchedule 함수
-    func fetchSchedule() {
-        database.collection("Schedule").whereField("groupName", isEqualTo: "허미니의또구동아리")
+    func fetchSchedule(gruopName: String) {
+        database.collection("Schedule").whereField("group_name", isEqualTo: gruopName)
             .getDocuments { (snapshot, error) in
                 self.schedule.removeAll()
                 
@@ -24,22 +24,23 @@ class ScheduleStore: ObservableObject {
                         let id: String = document.documentID
                         let docData = document.data()
                         
-                        let groupName: String = docData["groupName"] as? String ?? ""
-                        let lateFee: Int = docData["lateFee"] as? Int ?? 0
-                        let absenteeFee: Int = docData["absenteeFee"] as? Int ?? 0
+                        let groupName: String = docData["group_name"] as? String ?? ""
+                        let lateFee: Int = docData["late_fee"] as? Int ?? 0
+                        let absenteeFee: Int = docData["absentee_fee"] as? Int ?? 0
                         let location: String = docData["location"] as? String ?? ""
-                        let agreeTime: Int = docData["agreeTime"] as? Int ?? 0
+                        let agreeTime: Int = docData["agree_time"] as? Int ?? 0
                         let memo: String = docData["memo"] as? String ?? ""
                         
-                        let startTime: Timestamp = docData["startTime"] as? Timestamp ?? Timestamp()
+                        let startTime: Timestamp = docData["start_time"] as? Timestamp ?? Timestamp()
                         let startTimestamp: Date = startTime.dateValue()
                         
-                        let endTime: Timestamp = docData["endTime"] as? Timestamp ?? Timestamp()
+                        let endTime: Timestamp = docData["end_time"] as? Timestamp ?? Timestamp()
                         let endTimestamp: Date = endTime.dateValue()
                         
                         let schedule: Schedule = Schedule(id: id, groupName: groupName, lateFee: lateFee, absenteeFee: absenteeFee, location: location, startTime: startTimestamp, endTime: endTimestamp, agreeTime: agreeTime, memo: memo)
                         
                         self.schedule.append(schedule)
+                        print(schedule)
                     }
                 }
             }
@@ -50,21 +51,21 @@ class ScheduleStore: ObservableObject {
         do {
             try await database.collection("Schedule")
                 .document(schedule.id)
-                .setData(["groupName": schedule.groupName,
-                          "lateFee": schedule.lateFee,
-                          "absenteeFee": schedule.absenteeFee,
+                .setData(["group_name": schedule.groupName,
+                          "late_fee": schedule.lateFee,
+                          "absentee_fee": schedule.absenteeFee,
                           "location": schedule.location,
-                          "agreeTime": schedule.agreeTime,
+                          "agree_time": schedule.agreeTime,
                           "memo": schedule.memo,
-                          "startTime": schedule.startTime,
-                          "endTime": schedule.endTime,
+                          "start_time": schedule.startTime,
+                          "end_time": schedule.endTime,
                           "id": schedule.id
                          ])
         } catch {
             print("add schedule error: \(error.localizedDescription)")
         }
         
-        fetchSchedule()
+        fetchSchedule(gruopName: group.name)
         await addScheduleInGroup(schedule.id, group: group)
         // FIXME: - 일정을 만들때 해당하는 출석부도 만들어야 한다.
         // 연속으로 일정을 만드는 경우 이전일정은 저장은 안되는 문제도 존재
