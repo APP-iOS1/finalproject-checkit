1b5d4f92c9a129d857652c19e25e4d9e40248a96
Cho Hyunho <chohh02@gmail.com>
2023-02-01 18:09:53 +0900

    [Feat] CheckItCard
    카드 넘기는 뷰 수정작업

--- CheckIt/CheckIt/View/Check/CheckItCard.swift
+++ CheckIt/CheckIt/View/Check/CheckItCard.swift
@@ -8,68 +8,67 @@
 import SwiftUI
 
 struct CheckItCard: View {
-//    @Binding var isPresentedMapView: Bool 
-    @State var dDay: String = "D-day"
-    @State var groupName: String = "허니미니의 또구 동아리"
-    @State var place: String = "신촌 베이스볼클럽"
-    @State var date: String = "3월 24일"
-    @State var time: String = "오후 3:00 - 오후 7:00"
-    @State var groupImage: Image = Image("chocobi")
-    var isActiveButton: Bool = true
+    var data: Card
+    //    var isActiveButton: Bool = true
+    
     var body: some View {
         RoundedRectangle(cornerRadius: 10)
-            .frame(width: 330, height: 600)
-            .foregroundColor(.myLightGray)
+            .frame(width: 330, height: 500)
+            .foregroundColor(.gray)
             .overlay {
                 VStack(alignment: .center) {
-                   
-                        VStack(alignment: .leading) {
-                            HStack {
-                                TopSection
-                                Spacer()
-                            }
-                            
-                            HStack {
-                                InformationSection
-                                Spacer()
-                            }
-                        } // - VStack
-                        .frame(width: 280)
-                        .padding(10)
-                        
-                        
-                        //동아리 사진
-                        groupImage
-                            .resizable()
-                            .frame(width: 246, height: 186.81)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .padding(10)
-
-                        
-                    // Check It 버튼
-                    NavigationLink(destination: CheckMapView()) {
-                            CheckItButtonLabel(isActive: isActiveButton, text: "Check It!")
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        HStack {
+                            TopSection
+                            Spacer()
                         }
-                        .frame(width: 200)
-                        .padding(10)
-                        .disabled(!isActiveButton)
+                        
+                        HStack {
+                            InformationSection
+                            Spacer()
+                        }
+                    } // - VStack
+                    .frame(width: 280)
+                    .padding(10)
                     
-                        Spacer()
+                    
+                    //동아리 사진
+                    data.groupImage
+                        .resizable()
+                        .frame(width: 246, height: 186.81)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(10)
+                    
+                    
+                    // Check It 버튼
+                    NavigationLink(destination: ContentView()) {
+                        CheckItButtonLabel(isActive: data.isActiveButton, text: "Check It!")
+                    }
+                    .frame(width: 200)
+                    .padding(10)
+                    .disabled(!data.isActiveButton)
+                    
+                    Spacer()
                     
                 } // - VStack
+                .frame(width: UIScreen.main.bounds.width - 20, height: data.show ? 600 : 500)
+                .background(Color.white)
+                .cornerRadius(25)
             } // - overlay
+//            .padding(.leading, 20)
     }
     
     //MARK: - View(TopSection)
     private var TopSection: some View {
-            VStack(alignment: .leading) {
-                // 모임 날짜 나타내는 라벨
-                DdayLabel(dDay: dDay)
-                    .padding(.top, 10)
-                // 동아리 이름
-                Text("\(groupName)")
-                    .font(.title.bold())
-            } // - VStack
+        VStack(alignment: .leading) {
+            // 모임 날짜 나타내는 라벨
+            DdayLabel(dDay: data.dDay)
+                .padding(.top, 10)
+            // 동아리 이름
+            Text("\(data.groupName)")
+                .font(.title.bold())
+        } // - VStack
         
     } // - TopSection
     //MARK: - View(InformationSection)
@@ -78,32 +77,31 @@ struct CheckItCard: View {
             // 날짜
             HStack {
                 customSymbols(name: "calendar")
-                Text("\(date)")
+                Text("\(data.date)")
             } // - HStack
             .padding(.vertical, 3)
             
             // 시간
             HStack {
                 customSymbols(name: "clock")
-                Text("\(time)")
+                Text("\(data.time)")
             } // - HStack
             .padding(.vertical, 3)
             
             // 장소
             HStack {
                 customSymbols(name: "mapPin")
-                Text("\(place)")
+                Text("\(data.place)")
             } // - HStack
             .padding(.vertical, 3)
         } // - VStack
     } // - InformationSection
-    
 }
 
 
 
-struct CheckItCard_Previews: PreviewProvider {
-    static var previews: some View {
-        CheckItCard()
-    }
-}
+//struct CheckItCard_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CheckItCard(data: <#Card#>)
+//    }
+//}
