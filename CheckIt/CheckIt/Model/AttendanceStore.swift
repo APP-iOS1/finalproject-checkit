596160e3058f0241b0f76517addd9c79a24d8cf3
ryuchanghwi <ckdgnl10@naver.com>
2023-02-02 18:08:18 +0900

    [Style] - #63 동ㄹ아리 출석부 데이터 연결 작업

--- CheckIt/CheckIt/Model/AttendanceStore.swift
+++ CheckIt/CheckIt/Model/AttendanceStore.swift
@@ -11,20 +11,15 @@ import Firebase
 
 class AttendanceStore: ObservableObject {
     // 출석 명단 배열
-    @Published var attendanceList: [Attendance]
+    @Published var attendanceList: [Attendance] = []
     
     let database = Firestore.firestore()
-    
-    init(attendances: [Attendance] = []) {
-        self.attendanceList = attendances
-    }
     
     // 출석부 Fetch 함수
     func fetchAttendance(scheduleID: String) {
         attendanceList.removeAll()
         database.collection("Schedule").document("\(scheduleID)").collection("Attendance")
             .getDocuments { (snapshot, error) in
-                
                 if let snapshot {
                     for document in snapshot.documents {
                         let docData = document.data()
@@ -36,8 +31,39 @@ class AttendanceStore: ObservableObject {
                         let attendance = Attendance(id: id, uid: uid, attendanceStatus: attendanceStatus, settlementStatus: settlementStatus)
                         
                         self.attendanceList.append(attendance)
+                        print(attendance, "들어가는중")
+                    }
+                    print("끝")
+                }
+            }
+    }
+    func fetchUserAttendance(scheduleID: String, completion: @escaping (String) -> Void) {
+        attendanceList.removeAll()
+        database.collection("Schedule").document(scheduleID).collection("Attendance").whereField("uid", isEqualTo: "7h0Fg6Z6rgoEkRt5udPH") //임시 uid
+            .getDocuments { (snapshot, error) in
+                if let snapshot {
+                    for document in snapshot.documents {
+                        let docData = document.data()
+                        let id: String = docData["id"] as? String ?? ""
+                        let uid: String = docData["uid"] as? String ?? ""
+                        let attendanceStatus: String = docData["attendance_status"] as? String ?? ""
+                        let settlementStatus: Bool = docData["settlement_status"] as? Bool ?? true
+                        
+                        let attendance = Attendance(id: id, uid: uid, attendanceStatus: attendanceStatus, settlementStatus: settlementStatus)
+                        self.attendanceList.append(attendance)
+                        completion(scheduleID)
+                        print(scheduleID, "나와라")
+                        
                     }
                 }
             }
     }
+    func fetchUserAttendanceAsync(scheduleID: String) async -> String {
+        return await withCheckedContinuation { continuation in
+            fetchUserAttendance(scheduleID: scheduleID) { value in
+                continuation.resume(returning: value)
+            }
+        }
+    }
+    
 }
