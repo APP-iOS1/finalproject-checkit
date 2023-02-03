98894e2a76faa8e562dd1d1f9d03171a07cf05a4
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-02-04 01:16:01 +0900

    #102 - [Feat] 동아리 디테일 뷰에서 방장인지 아닌지 구분하는 기능 구현

--- CheckIt/CheckIt/View/Group/Main/GroupMainDetailView.swift
+++ CheckIt/CheckIt/View/Group/Main/GroupMainDetailView.swift
@@ -11,6 +11,15 @@ struct GroupMainDetailView: View {
     let group: Group
     let groupImage: UIImage
     
+    @EnvironmentObject var userStores: UserStore
+    
+    // FIXME: - 현재는 방장인지 아닌지만 여부를 나타내는데 운영진도 고려해야함
+    /// 현재 동아리가 자신이 방장인지 확인하는 연산 프로퍼티
+    /// true값이면 자신이 방장이며 fasle이면 방장이 아님
+    var isHost: Bool {
+        group.hostID == userStores.user?.id ?? ""
+    }
+    
     var body: some View {
         VStack {
             HStack {
@@ -23,18 +32,19 @@ struct GroupMainDetailView: View {
                         .frame(width: 85, height: 85)
                         .clipShape(Circle())
                     
-                    
-                    ZStack {
-                        // MARK: - 방장, 운영진 여부
-                        Circle()
-                            .fill(.white)
-                            .frame(width: 25, height: 25)
-                        
-                        Image(systemName: "crown.fill")
-                            .resizable()
-                            .frame(width: 20, height: 15)
-                            .foregroundColor(Color.myGreen)
-                        // FIXME: - 방장은 Color.myGreen, 운영진은 Color.myOrange로 바껴야함
+                    if isHost {
+                        ZStack {
+                            // MARK: - 방장, 운영진 여부
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 25, height: 25)
+                            
+                            Image(systemName: "crown.fill")
+                                .resizable()
+                                .frame(width: 20, height: 15)
+                                .foregroundColor(Color.myGreen)
+                            // FIXME: - 방장은 Color.myGreen, 운영진은 Color.myOrange로 바껴야함
+                        }
                     }
                 }
                 
@@ -60,5 +70,6 @@ struct GroupMainDetailView: View {
 struct GroupMainDetailView_Previews: PreviewProvider {
     static var previews: some View {
         GroupMainDetailView(group: Group.sampleGroup, groupImage: UIImage())
+            .environmentObject(UserStore())
     }
 }
