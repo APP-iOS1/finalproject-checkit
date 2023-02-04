93763a7339ec637f5ec799592c1b8a107e7a5c0c
Hyemin Heo <heohm128@gmail.com>
2023-02-05 04:02:01 +0900

    #82 - [Feat] KakaoLogin 구현

--- CheckIt/CheckIt/View/Login/LoginView.swift
+++ CheckIt/CheckIt/View/Login/LoginView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 import AuthenticationServices
 import GoogleSignIn
 import GoogleSignInSwift
+import KakaoSDKUser
 
 struct LoginView: View {
     @EnvironmentObject var userStore: UserStore
     var kakaoLoginButton: some View {
         Button(action: {
-
+            userStore.loginCenter = .kakao
+            userStore.loginWithKakao()
+                
+                 
         }, label: {
             HStack {
                 Image(systemName: "message.fill")
-
+                
                 Text(" 카카오로 로그인")
             }
             .frame(width: 280, height: 50)
@@ -27,17 +31,17 @@ struct LoginView: View {
         })
         .foregroundColor(.black)
     }
-
+    
     var naverLoginButton: some View {
         Button(action: {
-
+            
         }, label: {
             HStack {
                 Image("naver")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 40 ,height: 50)
-
+                
                 Text("네이버로 로그인")
             }
             .foregroundColor(.white)
@@ -46,14 +50,13 @@ struct LoginView: View {
             .cornerRadius(12)
         })
     }
-
+    
     // google login button custom
     var googleLoginButton: some View {
         Button(action: {
             userStore.loginCenter = .google
             Task {
-                let credential = await GoogleLoginStore().signIn()
-                await userStore.signInWithCredential(credential: credential)
+                await userStore.loginWithCredential()
             }
         })
         {
@@ -66,62 +69,58 @@ struct LoginView: View {
                         Image("google_logo")
                             .resizable()
                             .frame(width: 25, height: 25)
-
+                        
                         //FIXME: Robot 글꼴 적용해야함
                         Text("Sign in with Google")
                         Spacer()
                     }
                 }
-
+            
         }
-    }
-
-//    init() {
-//        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
-//            self.userStore.isPresentedLoginView = false
-//            Task {
-//                let credential = await GoogleLoginStore().restorePreviousLogin()
-////                await userStore.signInWithCredential(credential: credential)
-//                }
-//        }
-//    }
-
+    } // - googleLoginButton
+    
+    
     var body: some View {
         VStack {
             Spacer()
-
+            
             Image("CheckItLogo")
                 .resizable()
                 .scaledToFit()
-
+            
             //FIXME: - 수정 예정
             Text("동아리 관리는 Check - It")
                 .font(.title2)
-
+            
             Spacer()
-
+            
             SignInWithAppleButton(onRequest: { request in
                 request.requestedScopes = [.fullName, .email]
             }) { result in
-                let credential = AppleLoginStore().logIn(result: result)
+                userStore.loginCenter = .apple
                 Task {
-                    let isSuccessLogin = await userStore.signInWithCredential(credential: credential)
-                    if isSuccessLogin { userStore.loginCenter = .apple }
+                    await userStore.loginWithCredential(result)
                 }
             }
-                .frame(width: 280, height: 50)
-
+            .frame(width: 280, height: 50)
+            
             // 카카오 로그인
             kakaoLoginButton
-
+            
             // 구글 로그인
             googleLoginButton
-
-
+            
+            
+            Button(action: {
+                KakaoLoginStore().signOut()
+            }){
+                Text("카카오 로그아웃")
+            }
+            
             Spacer()
         }
     }
-
+    
     func signInWithGoogle() { }
 }
 
