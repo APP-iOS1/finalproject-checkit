4d107fefa796bdf0940812b8aed07e1bc22f9491
Hyemin Heo <heohm128@gmail.com>
2023-02-05 09:18:33 +0900

    #132 - [Feat/Style] 간편 회원가입 구현

--- CheckIt/CheckIt/View/Login/SignUpView.swift
+++ CheckIt/CheckIt/View/Login/SignUpView.swift
@@ -9,19 +9,22 @@ import SwiftUI
 
 struct SignUpView: View {
     @State var name: String = ""
+    @EnvironmentObject var userStore: UserStore
     var body: some View {
         VStack {
-            TextField("이름을 입력해주세요", text: $name)
-            
-            Button(action: {}) {
-                Text("Submit")
-            }
+            TextField("이름을 입력하세요", text: $name)
+            Button(action: {
+                userStore.changeUserName(name: name)
+                userStore.isFirstLogin = false
+                userStore.isPresentedLoginView = false
+            }) { Text("Submit") }
         }
     }
 }
 
-struct SignUpView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignUpView()
-    }
-}
+
+//struct SignUpView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SignUpView()
+//    }
+//}
