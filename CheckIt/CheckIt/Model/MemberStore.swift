c0d4414557962401605adf4f50626e0c53a6fe1c
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-02-06 13:28:07 +0900

    #149 - [Feat] 동아리에 해당하는 멤버를 fetch하는 메소드 구현

--- CheckIt/CheckIt/Model/MemberStore.swift
+++ CheckIt/CheckIt/Model/MemberStore.swift
@@ -6,9 +6,40 @@
 //
 
 import Foundation
+import Firebase
+
+enum MemberError: Error {
+    case notFoundMember
+}
 
 class MemberStore: ObservableObject {
+    let database = Firestore.firestore()
     
+    @Published var members: [Member] = []
+    
+    /// 동아리에 해당하는 구성원 정보를 불러오는 메소드입니다.
+    /// - Parameter groudId: 불러올 동아리 id
+    func fetchMembers(_ groupId: String) async throws {
+        let querySnapshot = try await database.collection("Group")
+            .document(groupId)
+            .collection("Member")
+            .getDocuments()
+        
+        guard !querySnapshot.isEmpty else { throw MemberError.notFoundMember }
+        
+        for documnet in querySnapshot.documents {
+            let data = documnet.data()
+            
+            let uid = data[MemberConstants.uid] as? String ?? ""
+            let position = data[MemberConstants.position] as? String ?? ""
+            
+            let member = Member(uid: uid, position: position)
+            
+            DispatchQueue.main.async {
+                self.members.append(member)
+            }
+        }
+    }
 }
 
 
