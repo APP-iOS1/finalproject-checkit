69bd0306dc7a0babf3bb7e1aac1f0be240306c63
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-02-06 15:41:10 +0900

    #149 - [Add] 운영진, 일반 동아리원이 보는 동아리 정보 뷰 추가

---
+++ CheckIt/CheckIt/View/Group/Information/General/GeneralGroupInfoMainView.swift
@@ -0,0 +1,20 @@
+//
+//  GeneralGroupInfoMainView.swift
+//  CheckIt
+//
+//  Created by 이학진 on 2023/02/06.
+//
+
+import SwiftUI
+
+struct GeneralGroupInfoMainView: View {
+    var body: some View {
+        Text("일반 동아리원이 보는 동아리 정보 뷰")
+    }
+}
+
+struct GeneralGroupInfoMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneralGroupInfoMainView()
+    }
+}
