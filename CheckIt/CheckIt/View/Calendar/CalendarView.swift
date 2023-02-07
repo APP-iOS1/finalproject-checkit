c7fd998cf71095f2e561a96d359c8bb7423793fa
blaire-pi <gpwls1969@cau.ac.kr>
2023-02-07 15:21:26 +0900

    [Feat]캘린더뷰 그룹 필터링

--- CheckIt/CheckIt/View/Calendar/CalendarView.swift
+++ CheckIt/CheckIt/View/Calendar/CalendarView.swift
@@ -7,52 +7,6 @@
 
 import SwiftUI
 
-//MARK: - 샘플 일정 데이터
-struct SampleTask: Identifiable {
-    var id = UUID().uuidString
-    var title: String
-    var time: Date = Date()
-}
-
-struct TaskMetaData: Identifiable {
-    var id = UUID().uuidString
-    var task: [SampleTask]
-    var taskDate: Date
-}
-
-func getSampleDate(offset: Int) -> Date {
-    let calendar = Calendar.current
-    
-    let date = calendar.date(byAdding: .day, value: offset, to: Date())
-    
-    return date ?? Date()
-}
-
-var tasks: [TaskMetaData] = [
-    TaskMetaData(task: [
-        SampleTask(title: "샘플1"),
-        SampleTask(title: "샘플2"),
-        SampleTask(title: "샘플3")
-    ], taskDate: getSampleDate(offset: 1)),
-    
-    TaskMetaData(task: [
-        SampleTask(title: "샘플4")
-    ], taskDate: getSampleDate(offset: -3)),
-    
-    TaskMetaData(task: [
-        SampleTask(title: "샘플5"),
-        SampleTask(title: "샘플6")
-    ], taskDate: getSampleDate(offset: -8)),
-    
-    TaskMetaData(task: [
-        SampleTask(title: "샘플7"),
-        SampleTask(title: "샘플8"),
-        SampleTask(title: "샘플9"),
-        SampleTask(title: "샘플10")
-    ], taskDate: getSampleDate(offset: 20))
-]
-
-//MARK: - 메인 뷰
 struct CalendarView: View {
     @EnvironmentObject var groupStore: GroupStore
     @EnvironmentObject var scheduleStore: ScheduleStore
@@ -70,31 +24,22 @@ struct CalendarView: View {
             Divider()
             
             //일정섹션
-            TaskView(currentDate: $currentDate, totalSchedule: $totalSchedule)
+            TaskView(currentDate: $currentDate, totalSchedule: $totalSchedule, selectedGroup: $selectedGroup)
             Spacer()
         }
         .padding(.vertical)
         .onAppear {
-            if selectedGroup != "전체" {
-                Task{
-                    await scheduleStore.fetchCalendarSchedule(groupName: selectedGroup)
-                    print("selectedGroup name: \(selectedGroup)")
-                    print("selectedGroup schedule: \(scheduleStore.scheduleList)")
-                }
-            } else {
                 groupStore.groups.forEach { group in
                     Task {
                         await scheduleStore.fetchCalendarSchedule(groupName: group.name)
                         print("foreach group name: \(group.name)")
-//                        totalSchedule.append(contentsOf: scheduleStore.scheduleList)
                         print("totalGroup schedule: \(scheduleStore.scheduleList)")
                         totalSchedule = scheduleStore.scheduleList
                     }
                 }
-            }
-            
         }
     }
+
 }
 
 struct CalendarView_Previews: PreviewProvider {
