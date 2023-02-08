de48a7d26a443ec18cd0f76d654bf7153f6f8c8a
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-02-08 22:57:17 +0900

    #218 - [Feat] 일정에 해당하는 출석부를 삭제하는 메소드 구현

--- CheckIt/CheckIt/Model/AttendanceStore.swift
+++ CheckIt/CheckIt/Model/AttendanceStore.swift
@@ -200,5 +200,18 @@ class AttendanceStore: ObservableObject {
         }
         return "error"
     }
+    
+    /// 일정을 삭제하는 메소드 입니다.
+    /// - Parameter attendanceId: 삭제할 출석의 id
+    func removeAttendance(_ scheduleId: String attendanceId: String) async {
+        do {
+            try await database.collection("Schedule").document(scheduleId)
+                .collection("Attendance")
+                .document(attendanceId)
+                .delete()
+        } catch {
+            print("removeAttendance error: \(error.localizedDescription)")
+        }
+    }
 
 }
