3bab647005788fad51156c698256554d09c9c698
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-02-08 22:49:44 +0900

    #218 - [Feat] 스케줄 id를 가지고 해당 출석부를 가져오는 메소드 구현

--- CheckIt/CheckIt/Model/AttendanceStore.swift
+++ CheckIt/CheckIt/Model/AttendanceStore.swift
@@ -183,6 +183,22 @@ class AttendanceStore: ObservableObject {
         }
         return false
     }
-
     
+    /// 일정에 해당하는 스케줄의 id를 얻는 메소드 입니다.
+    /// - Parameter scheduleId: 가져올 출석이랑 연관된 일정
+    /// - Returns: 출석의 id
+    func getAttendanceId(_ scheduleId: String) async -> String {
+        do {
+           let querySnapshot = try await database.collection("Schedule").document(scheduleId)
+                .collection("Attendance")
+                .getDocuments()
+            if querySnapshot.isEmpty { return "Error"}
+            let document = querySnapshot.documents.first!
+            return document.documentID
+        } catch {
+            print("getAttendance error: \(error.localizedDescription)")
+        }
+        return "error"
+    }
+
 }
