e8cd700ded98453f54de3e5b877df453444f3daf
hwangyeri <dpfl420@naver.com>
2023-02-09 16:15:12 +0900

    #231 - extension UIScreen 추가

---
+++ CheckIt/CheckIt/Extensions/Ex+UIScreen.swift
@@ -0,0 +1,14 @@
+//
+//  Ex+UIScreen.swift
+//  CheckIt
+//
+//  Created by 황예리 on 2023/02/09.
+//
+
+import Foundation
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
