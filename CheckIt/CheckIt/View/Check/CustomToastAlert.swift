955db9e19fca0fe291c2518fa58bfe70b1f2ddc9
Hyemin Heo <heohm128@gmail.com>
2023-02-13 01:05:12 +0900

    [Refactor] 출석 버튼 활성화 로직 일부 구현

--- CheckIt/CheckIt/View/Check/CustomToastAlert.swift
+++ CheckIt/CheckIt/View/Check/CustomToastAlert.swift
@@ -7,18 +7,25 @@
 
 import SwiftUI
 
+//MARK: - Enum(AlertMode)
+enum AlertMode: String {
+    case time
+    case place
+    case complete
+}
+
+//MARK: - View(CustomToastAlert)
 struct CustomToastAlert: View {
-    @Binding var distance: Double
+    
+    @State var distance: Double = 0.0
     @Binding var isPresented: Bool
+    @Binding var mode: AlertMode
+    
     var body: some View {
         if isPresented {
             toastAlertLabel
                 .overlay {
-                    VStack {
-                        Text("모임 장소로 이동해주세요.")
-                        Text("모임 위치까지") + Text(" \(distance)m ").underline() + Text("남았습니다.")
-                            
-                    }
+                   displayText
                 }
                 .padding(.vertical, 80)
                 .onAppear {
@@ -29,11 +36,32 @@ struct CustomToastAlert: View {
         }
     }
     
+    //MARK: - View(displayText)
+    private var displayText: some View {
+        switch mode {
+        case .time:
+            return AnyView(Text("출석체크 시간이 아닙니다!"))
+        case .place:
+            return AnyView(
+            VStack {
+            Text("모임 장소로 이동해주세요.")
+            Text("모임 위치까지") + Text(" \(distance)m ").bold() + Text("남았습니다.")
+            })
+        case .complete:
+            return AnyView(Text("출석이 이미 완료되었습니다."))
+        default:
+            return AnyView(Text(""))
+        }
+        AnyView(Text(""))
+    } // - displayText
     
+    
+    
+    //MARK: - toastAlertLabel
     private var toastAlertLabel: some View {
         RoundedRectangle(cornerRadius: 20)
             .foregroundColor(.toastAlertGray)
             .frame(width: 280, height: 80)
-    }
+    } // - toastAlertLabel
 }
 
