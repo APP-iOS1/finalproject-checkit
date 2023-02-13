dc388e09c728b9084d78721023ba86d486dcf3de
hwangyeri <dpfl420@naver.com>
2023-02-13 17:35:37 +0900

    #311 - OnboardingView 생성 및 연결 작업

--- CheckIt/CheckIt/View/ContentView.swift
+++ CheckIt/CheckIt/View/ContentView.swift
@@ -11,53 +11,61 @@ struct ContentView: View {
     @EnvironmentObject var userStore: UserStore
     @EnvironmentObject var groupStore: GroupStore
     @EnvironmentObject var scheduleStore: ScheduleStore
+    
+    @AppStorage("onboarding") var isOnboarindViewActive: Bool = true
+    
     var body: some View {
-//        OnBoardingView()
         
-            TabView {
-                
-                CheckMainView()
-                    .tabItem {
-                        Image(systemName: "checkmark.seal.fill")
-                        Text("출석체크")
-                    }
-                GroupMainView()
-                    .tabItem {
-                        Image(systemName: "house.fill")
-                        Text("동아리")
-                    }
-                
-                CalendarView()
-                    .tabItem {
-                        Image(systemName: "calendar")
-                        Text("캘린더")
-                    }
-            }
-            .accentColor(Color.myGreen)
-            .onAppear {
-                UITabBar.appearance().backgroundColor = .lightGray
-
-                guard let user = userStore.user else { return }
-                if userStore.isLogined {
-                    return
+        ZStack {
+            if isOnboarindViewActive {
+                OnBoardingView()
+            } else {
+                TabView {
+                    
+                    CheckMainView()
+                        .tabItem {
+                            Image(systemName: "checkmark.seal.fill")
+                            Text("출석체크")
+                        }
+                    GroupMainView()
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                            Text("동아리")
+                        }
+                    
+                    CalendarView()
+                        .tabItem {
+                            Image(systemName: "calendar")
+                            Text("캘린더")
+                        }
                 }
-                //UITabBar.appearance().backgroundColor = .black
-                
-                userStore.isLogined.toggle()
-                
-                Task {
-                    await groupStore.fetchGroups(user)
-                    print("groups: \(groupStore.groups)")
-                    for group in groupStore.groups {
-                        await scheduleStore.fetchRecentSchedule(groupName: group.name)
+                .accentColor(Color.myGreen)
+                .onAppear {
+                    UITabBar.appearance().backgroundColor = .lightGray
+                    
+                    guard let user = userStore.user else { return }
+                    if userStore.isLogined {
+                        return
                     }
+                    //UITabBar.appearance().backgroundColor = .black
+                    
+                    userStore.isLogined.toggle()
+                    
+                    Task {
+                        await groupStore.fetchGroups(user)
+                        print("groups: \(groupStore.groups)")
+                        for group in groupStore.groups {
+                            await scheduleStore.fetchRecentSchedule(groupName: group.name)
+                        }
+                    }
+                    userStore.startUserListener(user.id)
+                    print("groups : \(groupStore.groups)")
                 }
-                userStore.startUserListener(user.id)
-                print("groups : \(groupStore.groups)")
+                .onDisappear {
+                    groupStore.detachListener()
+                }
             }
-            .onDisappear {
-                groupStore.detachListener()
-            }
+        }
     }
 }
 
